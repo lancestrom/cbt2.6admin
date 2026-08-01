bf4ea40d8ce89e5db7e3c156bdc078bf619f1cee
RAHMADIKA SETIAWAN <dikakewren@gmail.com>
2026-08-01 20:28:36 +0700

    MVC UPDATE

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="author" content="rahmadika surya setiawan">
 
     <title><?= $title ?></title>
     <link rel="icon" type="image/png" href="<?= base_url() ?>assets/images/logo.png" />
